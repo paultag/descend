0c80977f7fa6d9086b5f4bf711f116ed4ef7bd1f
Paul Tagliamonte <paultag@gmail.com>
2015-08-31 16:44:41 -0400

    reflow to add map[string]string back

--- descend/descend.go
+++ descend/descend.go
@@ -1,6 +1,7 @@
 package descend
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
@@ -9,7 +10,7 @@ import (
 	"pault.ag/go/debian/control"
 )
 
-func DputFile(client *http.Client, host, archive, fpath string) error {
+func DputFile(client *http.Client, host, archive, fpath string) (error, map[string]string) {
 	filename := path.Base(fpath)
 	req, err := http.NewRequest(
 		"PUT",
@@ -18,24 +19,27 @@ func DputFile(client *http.Client, host, archive, fpath string) error {
 	)
 
 	if err != nil {
-		return err
+		return err, map[string]string{}
 	}
 	fd, err := os.Open(fpath)
 	if err != nil {
-		return err
+		return err, map[string]string{}
 	}
 	req.Body = fd
-	_, err = client.Do(req)
+	resp, err := client.Do(req)
 	if err != nil {
-		return err
+		return err, map[string]string{}
 	}
-	return nil
+
+	var reply map[string]string
+	json.NewDecoder(resp.Body).Decode(&reply)
+	return nil, reply
 }
 
 func DoPutChanges(client *http.Client, changes *control.Changes, host, archive string) error {
 	root := path.Dir(changes.Filename)
 	for _, file := range changes.Files {
-		err := DputFile(client, host, archive, path.Join(root, file.Filename))
+		err, _ := DputFile(client, host, archive, path.Join(root, file.Filename))
 		if err != nil {
 			return err
 		}
